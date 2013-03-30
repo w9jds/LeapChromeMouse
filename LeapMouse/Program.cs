8a0b368fd8588bd5f2822e16cf22ce21940f261d
Jeremy Shore <xboxmusicn3rd@gmail.com>
2013-03-30 01:34:26 -0500

    Real update using new gestures
    Got rid of my crappy ones and used the ones built into sdk.
    Doesn't jump more than one tab as often as first one (still does do it)

--- LeapMouse/Program.cs
+++ LeapMouse/Program.cs
@@ -8,17 +8,20 @@ using System.Runtime.InteropServices;
 
 class LeapListener : Listener
 {
+    private Frame lastFrame = new Frame();
+
     [DllImport("user32.dll", SetLastError = true)]
     static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
 
-    public const int KEYEVENTF_EXTENDEDKEY = 0x01; //Key down flag
-    public const int KEYEVENTF_KEYUP = 0x02; //Key up flag
-    public const int VK_LCONTROL = 0xA2; //Left Control key code
-    public const int VK_TAB = 0x09; //Tab key code
-    public const int VK_SHIFT = 0x10; //Shift key code
+    private const int KEYEVENTF_EXTENDEDKEY = 0x01; //Key down flag
+    private const int KEYEVENTF_KEYUP = 0x02; //Key up flag
+    private const int VK_LCONTROL = 0xA2; //Left Control key code
+    private const int VK_TAB = 0x09; //Tab key code
+    private const int VK_SHIFT = 0x10; //Shift key code
 
     public override void OnInit(Controller controller) {
         Console.WriteLine("Initialized");
+        controller.EnableGesture(Gesture.GestureType.TYPESWIPE);
     }
 
     public override void OnConnect(Controller controller) {
@@ -37,23 +40,24 @@ class LeapListener : Listener
     {
         //System.Threading.Thread.Sleep(50);
         Frame frame = controller.Frame();
-        Console.WriteLine("Frame id: " + frame.Id + ", timestamp: " + frame.Timestamp + ", hands: " + frame.Hands.Count + ", fingers: " + frame.Fingers.Count + ", tools: " + frame.Tools.Count);
+        
+        //Console.WriteLine("Frame id: " + frame.Id + ", timestamp: " + frame.Timestamp + ", hands: " + frame.Hands.Count + ", fingers: " + frame.Fingers.Count + ", tools: " + frame.Tools.Count);
 
-        if (!frame.Hands.Empty) 
+        if (lastFrame == frame)
+            return;
+
+        GestureList Gestures =  (lastFrame.IsValid == true) ? frame.Gestures(lastFrame) : frame.Gestures();
+
+        lastFrame = frame;
+
+        for (int i = 0; i < Gestures.Count; i++)
         {
-            // Get the first hand
-            Hand hand = frame.Hands[0];
-            FingerList fingers = hand.Fingers;
-            Vector handVector = hand.PalmNormal;
-            if (!fingers.Empty)
+            if (Gestures[i].Type == Gesture.GestureType.TYPESWIPE && Gestures[i].State == Gesture.GestureState.STATESTOP)
             {
-                var fingerVelocity = fingers.Average(f => f.TipVelocity.x);
-                var fingerVelocityVariance = fingers.Sum(f => Math.Pow(f.TipVelocity.x - fingerVelocity, 2)) / fingers.Count();
-                var fingerVelocityDeviation = (float)Math.Sqrt(fingerVelocityVariance);
-
-                if ((handVector.Yaw * 57.295779513f) > 25 && fingerVelocity > 300 && fingerVelocityDeviation > 40)
+                SwipeGesture Swipe = new SwipeGesture(Gestures[i]);
+                if (Swipe.Direction.x < (Vector.Right.x + .1) && Swipe.Direction.x > (Vector.Right.x - .1))
                 {
-                    Console.WriteLine("swipe left");
+                    Console.WriteLine("Swipe Right");
                     keybd_event(VK_LCONTROL, 0, 0, 0);
                     keybd_event(VK_SHIFT, 0, 0, 0);
                     keybd_event(VK_TAB, 0, 0, 0);
@@ -62,44 +66,20 @@ class LeapListener : Listener
                     keybd_event(VK_SHIFT, 0, KEYEVENTF_KEYUP, 0);
                     keybd_event(VK_LCONTROL, 0, KEYEVENTF_KEYUP, 0);
                     System.Threading.Thread.Sleep(50);
-                }
 
-                else if ((handVector.Yaw * 57.295779513f) < -25 && fingerVelocity < -300 && fingerVelocityDeviation > 40)
+                }
+                if (Swipe.Direction.x < (Vector.Left.x + .1) && Swipe.Direction.x > (Vector.Left.x - .1))
                 {
-                    Console.WriteLine("swipe right");
+                    Console.WriteLine("Swipe Left");
                     keybd_event(VK_LCONTROL, 0, 0, 0);
                     keybd_event(VK_TAB, 0, 0, 0);
 
                     keybd_event(VK_TAB, 0, KEYEVENTF_KEYUP, 0);
                     keybd_event(VK_LCONTROL, 0, KEYEVENTF_KEYUP, 0);
                     System.Threading.Thread.Sleep(50);
+
                 }
             }
-                 
-            // Check if the hand has any fingers
-            /*
-            FingerList fingers = hand.Fingers;
-            if (!fingers.Empty) 
-            {
-                // Calculate the hand's average finger tip position
-                Vector avgPos = new Vector();
-                for (int i = 0; i < fingers.Count(); ++i)
-                    avgPos += fingers[i].TipPosition;
-                
-                avgPos /= (float)fingers.Count;
-                Console.WriteLine("Hand has " + fingers.Count + " fingers, average finger tip position" + avgPos);
-            }
-                
-            // Get the hand's sphere radius and palm position
-            Console.WriteLine("Hand sphere radius: " + hand.SphereRadius + " mm, palm position: " + hand.PalmPosition);
-
-            // Get the hand's normal vector and direction
-            Vector normal = hand.PalmNormal;
-            Vector direction = hand.Direction;
-
-            // Calculate the hand's pitch, roll, and yaw angles
-            Console.WriteLine("Hand pitch: " + direction.Pitch * 57.295779513f + " degrees, roll: " + normal.Roll * 57.295779513f + " degrees, yaw: " + direction.Yaw * 57.295779513f + " degrees");
-            */
         }
     }
 }
@@ -116,7 +96,6 @@ class LeapMouse
         Console.ReadLine();
 
         controller.RemoveListener(listener);
-        
     }
 }
 
